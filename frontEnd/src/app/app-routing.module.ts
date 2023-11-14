6f4decb0b362901f131b65b859b5915d305e24f3
Jaishree-V <shree7102000@gmail.com>
2023-11-14 15:06:16 +0530

    Jai Shree

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerLoginComponent } from './customer/customer-login/customer-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: CustomerLoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontEnd/src/app/customer/customer-login/customer-login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-login',
+  templateUrl: './customer-login.component.html',
+  styleUrls: ['./customer-login.component.css']
+})
+export class CustomerLoginComponent {
+
+  username: string='';
+  password: string='';
+  login(){
+
+  }
+}
