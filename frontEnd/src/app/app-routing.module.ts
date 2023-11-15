d04698796f146494360d4201252bb9696973263d
lousychad <kirupa243@gmail.com>
2023-11-15 14:38:31 +0530

    admin,login,home page

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomerLoginComponent } from './customer/customer-login/customer-login.component';
 import { CustomerHomeComponent } from './customer/customer-home/customer-home.component';
+import { CustomerRegistrationComponent } from './customer/customer-registration/customer-registration.component';
+import { AdminhomeComponent } from './admin/adminhome/adminhome.component';
+import { AdminloginComponent } from './admin/adminlogin/adminlogin.component';
 
 const routes: Routes = [
   {path: "", component: CustomerLoginComponent},
-  {path:"customerHome",component:CustomerHomeComponent}
+  {path:"customerHome",component:CustomerHomeComponent},
+  {path: "customerRegistration", component: CustomerRegistrationComponent},
+  {path:"adminHome", component: AdminhomeComponent},
+  {path:"adminlogin", component: AdminloginComponent}
 ];
 
 @NgModule({
