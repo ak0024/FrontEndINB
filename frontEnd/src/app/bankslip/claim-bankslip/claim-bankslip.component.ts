0b5e109e781854caf7f20b2b36f6bc10b16fbbde
Dhanu1508 <dhanapavi15@gmail.com>
2023-11-17 11:33:37 +0530

    ddd

--- frontEnd/src/app/bankslip/claim-bankslip/claim-bankslip.component.ts
+++ frontEnd/src/app/bankslip/claim-bankslip/claim-bankslip.component.ts
@@ -11,6 +11,10 @@ import { Router } from '@angular/router';
 })
 export class ClaimBankslipComponent implements OnInit{
   claimbankslip: BankSlip[]=[];
+  bankSlip :BankSlip = new BankSlip();
+  showMessage: boolean = false;
+  checknumber:string="";
+
   constructor(private bankSlipServiceService:BankslipServiceService, private router:Router){}
 
 ngOnInit(): void{
@@ -24,5 +28,14 @@ ngOnInit(): void{
         console.log(data);
       }
     )
+  }
+  Message(claimbankslip:BankSlip) {
+    this.bankSlip.slipstatus="In progress"
+    this.bankSlipServiceService.updateBankslip(this.bankSlip).subscribe(
+      data=>{
+        console.log(data)
+        this.showMessage=true
+      }
+    );
   }
 }
