07fe7c7ab64fa591e4af8617961650d1b83f5fd2
ak0024 <ashokkumar241100@gmail.com>
2023-11-19 14:07:31 +0530

    ffe

--- frontEnd/src/app/customer/customer-registration/customer-registration.component.ts
+++ frontEnd/src/app/customer/customer-registration/customer-registration.component.ts
@@ -15,16 +15,33 @@ export class CustomerRegistrationComponent implements OnInit {
   cities: string[] = [];
   selectedState: string = '';
 
+
+  firstNameReq: boolean = false;
+  lastNameReq: boolean = false;
+  address1Req: boolean = false;
+  stateReq: boolean = false;
+  cityReq: boolean = false;
+  zipReq: boolean = false;
+  phoneReq: boolean = false;
+  emailReq: boolean = false;
+  accountTypeReq: boolean = false;
+  userNameReq: boolean = false;
+  passwordReq: boolean = false;
+  confirmPasswordReq: boolean = false;
+
+
+
   constructor(
     private customerRegistrationService: CustomerRegistrationServiceService,
     private dataService: DataServiceService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getStates();
   }
   account: Account = new Account();
+  confirmPassword: string = ""
 
   getStates() {
     this.dataService.getStates().subscribe((states: string[]) => {
@@ -47,6 +64,45 @@ export class CustomerRegistrationComponent implements OnInit {
   }
 
   register() {
+    if (this.account.customer_id.firstName === "") {
+      this.firstNameReq = true
+    }
+    if (this.account.customer_id.lastName === "") {
+      this.lastNameReq = true
+    }
+    if (this.account.customer_id.addressLine1 === "") {
+      this.address1Req = true
+    }
+    if (this.account.customer_id.state === "") {
+      this.stateReq = true
+    }
+    if (this.account.customer_id.state !== "" && this.account.customer_id.city === "") {
+      this.cityReq = true
+    }
+    if(this.account.customer_id.zip===0){
+      this.zipReq=true
+    }
+    if (this.account.customer_id.phone === 0) {
+      this.phoneReq = true
+    }
+    if (this.account.customer_id.email === "") {
+      this.emailReq = true
+    }
+    if (this.account.account_type === "") {
+      this.accountTypeReq = true
+    }
+    if (this.account.customer_id.userName === "") {
+      this.userNameReq = true
+    }
+    if (this.account.customer_id.password === "") {
+      this.passwordReq = true
+    }
+    if (this.confirmPassword === "") {
+      this.confirmPasswordReq = true
+    }
+
+
+    this.router.navigate(['customerRegistration']);
     console.log(this.account);
     this.customerRegistrationService
       .createCustomerRegistration(this.account)
