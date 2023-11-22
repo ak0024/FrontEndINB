b06640ce3603190ab3bcf39d7db1766d8094fa84
Aswin-B19 <aswin1910@gmail.com>
2023-11-22 14:26:05 +0530

    admin over

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -16,6 +16,9 @@ import { LogoutComponent } from './customer/logout/logout.component';
 import { CustomerHomeComponent } from './customer/customer-home/customer-home.component';
 import { CustomerapprovalComponent } from './admin/customerapproval/customerapproval.component';
 import { CustomerViewComponent } from './customer/customer-view/customer-view.component';
+import { SendMoneyComponent } from './customer/send-money/send-money.component';
+import { BankSlipApprovalComponent } from './admin/bank-slip-approval/bank-slip-approval.component';
+import { AccountapprovalComponent } from './admin/accountapproval/accountapproval.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +35,10 @@ import { CustomerViewComponent } from './customer/customer-view/customer-view.co
     EditProfileComponent,
     LogoutComponent,
     CustomerapprovalComponent,
-    CustomerViewComponent
+    CustomerViewComponent,
+    SendMoneyComponent,
+    BankSlipApprovalComponent,
+    AccountapprovalComponent
   ],
   imports: [
     BrowserModule,
