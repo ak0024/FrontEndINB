d8f029a0cf2a177228a03a5082f141e752c5eba5
Jaishree-V <shree7102000@gmail.com>
2023-11-24 10:40:45 +0530

    nnnn

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -20,7 +20,9 @@ import { SendMoneyComponent } from './customer/send-money/send-money.component';
 import { AccountapprovalComponent } from './admin/accountapproval/accountapproval.component';
 import { BankSlipApprovalComponent } from './admin/bank-slip-approval/bank-slip-approval.component';
 import { PasswordReachLimitComponent } from './admin/password-reach-limit/password-reach-limit.component';
-import { SendMoneyComponent } from './customer/send-money/send-money.component';
+import { CustomerbankslipComponent } from './customer/customerbankslip/customerbankslip.component';
+import { IssueBankSlipComponent } from './bankslip/issue-bankSlip/issue-bankSlip.component';
+
 
 
 @NgModule({
@@ -43,6 +45,8 @@ import { SendMoneyComponent } from './customer/send-money/send-money.component';
     AccountapprovalComponent,
     BankSlipApprovalComponent,
     PasswordReachLimitComponent,
+    CustomerbankslipComponent,
+    IssueBankSlipComponent,
   ],
   imports: [
     BrowserModule,
